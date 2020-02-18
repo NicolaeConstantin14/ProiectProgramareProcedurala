e8099daa8507a414eac4e4a27c1f62d3487f4e62
Nicolae Constantin <constantinic94@gmail.com>
2020-02-18 18:45:26 +0200

    Commit pentru completare tema 4

--- Tema4/main/TestImobil.java
+++ Tema4/main/TestImobil.java
@@ -3,9 +3,13 @@ package main;
 public class TestImobil {
 
 	public static void main(String[] args) {
-		Imobil imobil = new Imobil();
+		Imobil imobil1 = new Imobil();
+		Imobil imobil2 = new Imobil("apartament", 1, 60000);
+		Imobil imobil3 = new Imobil("vila", 3, 100000);
 		
-		System.out.println("Tip Casa: " + imobil.getTipCasa() + "\nNumar etaje: " + imobil.getNrEtaje() + "\nPret: " + imobil.getPret());
+		System.out.println("Tip Casa: " + imobil1.getTipCasa() + "\nNumar etaje: " + imobil1.getNrEtaje() + "\nPret: " + imobil1.getPret()+ "\n");
+		System.out.println("Tip Casa: " + imobil2.getTipCasa() + "\nNumar etaje: " + imobil2.getNrEtaje() + "\nPret: " + imobil2.getPret()+ "\n");
+		System.out.println("Tip Casa: " + imobil3.getTipCasa() + "\nNumar etaje: " + imobil3.getNrEtaje() + "\nPret: " + imobil3.getPret()+ "\n");
 		
 	}
 
